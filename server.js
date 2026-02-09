444105e8b914caf03e2ce045ed6dd3867e862a64
Paola-1599 <anyi_pgomeza@soy.sena.edu.co>
2026-02-09 14:18:07 -0500

    cambio en server

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 
 // CORS - Permitir peticiones desde el frontend
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://park-sync.netlify.app',
   credentials: true
 }));
 
